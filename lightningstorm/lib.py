202033980832f56707b13863a1b1073d91b06356
David Kwast <david@kwast.net>
2016-08-21 00:08:49 -0300

    lib.py

--- lightningstorm/lib.py
+++ lightningstorm/lib.py
@@ -1,2 +1,23 @@
-# encoding utf-8
 
+class Storage:
+    'Abstract Base Class for node storage'
+    
+    def store_raindrop(self, metadata, payload):
+        'returns raindrop UUID'
+        raise NotImplementedError
+    
+    def retrieve_raindrop(self, uuid):
+        'returns a tuple containing metadata and file-like object represent the payload'
+        raise NotImplementedError
+    
+    def search_raindrops(self, query):
+        'searches for raindrops'
+        raise NotImplementedError
+    
+    def list_raindrops(self, offset=0, limit=10):
+        "returns a list of raindrop's UUIDs"
+        raise NotImplementedError
+
+class DevelopmentStorage(Storage):
+    def __init__(self):
+        pass
